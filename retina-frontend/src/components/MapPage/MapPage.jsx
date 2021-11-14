78c8a06c159a5e2be3922db298c0df8e662383a8
Sarmueil <adedoyin.adebisi@decagon.dev>
2021-11-14 23:25:53 +0100

    home-page-bug-fix

--- retina-frontend/src/components/MapPage/MapPage.jsx
+++ retina-frontend/src/components/MapPage/MapPage.jsx
@@ -12,7 +12,7 @@ import { format } from 'timeago.js'
  
 const MapPage = () => {
   const [pins, setPins] = useState([])
-
+   const [pinid, setPinid] = useState(null)
   useEffect(()=>{
     const getPins = async()=>{
        try{
@@ -24,6 +24,9 @@ const MapPage = () => {
     }
     getPins()
   })
+  const handleClick =(id)=>{
+    setPinid(id)
+  }
     const [viewport, setViewport] = useState({   
         width: "100vw",
         height: "100vh",
@@ -42,47 +45,50 @@ const MapPage = () => {
       {pins.map((pin)=>(
          <div key={pin._id}>
          <Marker latitude={pin.lat} longitude={pin.long} offsetLeft={-20} offsetTop={-10}>
-       <RoomIcon style={{fontSize:viewport.zoom * 7, color:"red"}} />
+       <RoomIcon style={{fontSize:viewport.zoom * 7, color:"red", cursor:'pointer'}} onClick={()=>handleClick(pin._id)}/>
       </Marker>
-      <Popup
-          latitude={pin.lat}  
-          longitude={pin.long}
-          closeButton={true}
-          closeOnClick={false}
-          anchor="left" >
-            <div className="flex justify-center items-center flex-col">
-          <div className="bg-transparent rounded-2xl h-96" style={{width:'40vw'}}>    
-             <div className="bg-black w-full rounded-xl" >
-              <video autoPlay loop muted className="object-contain h-96">
-              <source src={Mapvid} type="video/mp4"/>
-              </video>
-             </div> 
-          </div>
-          <div className="bg-white pt-3 pb-3" style={{width:'40vw'}}>
-               <div className="h-full">
-                 <div className="flex justify-between items-center">
-                      <div className="flex items-center">
-                         <Avatar alt="Remy Sharp" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
-                         <h3 className="text-base tracking-wide ml-2 text-black">{pin.postedBy}</h3>
-                      </div>
-                      <MoreHorizIcon />
-                 </div>
-                 <p className="text-sm tracking-wide ml-10 text-black">{pin.location}</p>
-                  <p className="text-base tracking-wide ml-2 text-black mt-3">{pin.caption}</p>
-                  <div className="flex justify-between items-center">
-                  <div className="flex items-center mt-3">
-                     <StarIcon style={{color:'gold'}}/>
-                     <StarIcon style={{color:'gold'}}/>
-                     <StarIcon style={{color:'gold'}}/>
-                     <StarIcon style={{color:'gold'}}/>  
-                     <StarIcon style={{color:'gold'}}/>
-                  </div>
-                     <h3 className="text-base mr-4">{format(pin.createdAt)}</h3>
-                  </div>
+      {pin._id === pinid && (
+        <Popup
+        latitude={pin.lat}  
+        longitude={pin.long}
+        closeButton={true}
+        closeOnClick={false}
+        onClose={()=>setPinid(null)}
+        anchor="left" >
+          <div className="flex justify-center items-center flex-col">
+        <div className="bg-transparent rounded-2xl h-96" style={{width:'40vw'}}>    
+           <div className="bg-black w-full rounded-xl" >
+            <video autoPlay loop muted className="object-contain h-96"> 
+            <source src={Mapvid} type="video/mp4"/>
+            </video>
+           </div> 
+        </div>
+        <div className="bg-white pt-3 pb-3" style={{width:'40vw'}}>
+             <div className="h-full">
+               <div className="flex justify-between items-center">
+                    <div className="flex items-center">
+                       <Avatar alt="Remy Sharp" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+                       <h3 className="text-base tracking-wide ml-2 text-black">{pin.postedBy}</h3>
+                    </div>
+                    <MoreHorizIcon />
                </div>
+               <p className="text-sm tracking-wide ml-10 text-black">{pin.location}</p>
+                <p className="text-base tracking-wide ml-2 text-black mt-3">{pin.caption}</p>
+                <div className="flex justify-between items-center">
+                <div className="flex items-center mt-3">
+                   <StarIcon style={{color:'gold'}}/>
+                   <StarIcon style={{color:'gold'}}/>
+                   <StarIcon style={{color:'gold'}}/>
+                   <StarIcon style={{color:'gold'}}/>  
+                   <StarIcon style={{color:'gold'}}/>
+                </div>
+                   <h3 className="text-base mr-4">{format(pin.createdAt)}</h3>
+                </div>
              </div>
-          </div>
-        </Popup>
+           </div>
+        </div>
+      </Popup>
+      )}
         </div>
       ))}
       
